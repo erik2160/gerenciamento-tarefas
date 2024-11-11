08a0d9e27e601c7cb18e96957e5016ae19d5c005
Erik Fernandes <erikfesilva@gmail.com>
2024-11-11 14:49:19 -0300

    initial files

---
+++ api/proa/src/test/java/tarefas/gerenciamento/proa/ProaApplicationTests.java
@@ -0,0 +1,13 @@
+package tarefas.gerenciamento.proa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
